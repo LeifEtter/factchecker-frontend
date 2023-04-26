363923e73fb9e4410ede2b0560bb18b6c2f092f9
Leif Etter <leif.etter@gmx.de>
2023-04-26 23:16:39 +0200

    add medium dark text color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     extend: {
       colors: {
         "fact-text-light": "#535353",
+        "fact-text-medium": "#1B2636",
         "fact-text-dark": "#000000",
         "fact-text-red": "#3F0202",
         "fact-blue": "#1B5BFD",
