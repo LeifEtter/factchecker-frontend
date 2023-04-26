fca00afb49482684edb8bacc6d8e968b9ee7c54c
Leif Etter <leif.etter@gmx.de>
2023-04-26 22:11:10 +0200

    import roboto mono font and utilize nextjs font hosting to provide the font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/shadows.css";
+import { Roboto_Mono } from "next/font/google";
+
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={robotoMono.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
