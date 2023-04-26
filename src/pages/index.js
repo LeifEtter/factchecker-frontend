8732bc85eda57a5c11e0a5d7b9e64d19b5cc7a8b
Leif Etter <leif.etter@gmx.de>
2023-04-26 19:26:44 +0200

    extract claim card component into seperate file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { ClaimCard } from "@/components/ClaimCard";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -26,50 +27,3 @@ export default function Home() {
     </main>
   );
 }
-
-const ClaimCard = ({ title, description }) => (
-  <div className="flex flex-col bg-white p-3 rounded-2xl special-shadow">
-    <div className="flex items-center justify-between">
-      <h1>{title}</h1>
-      <IndicatorNew validity={35} />
-    </div>
-    <div>{description}</div>
-    <div className="flex flex-row gap-4">
-      <div className="flex-1 relative">
-        <Image
-          src="https://factchecker-images.s3.eu-central-1.amazonaws.com/claims/trump_arrested.jpg"
-          alt={`${title}-image`}
-          fill
-          className="object-cover rounded-2xl"
-        />
-      </div>
-      <div className="flex-1">
-        <div className="rounded-xl p-2 shadow-md">Evidence</div>
-        <div className="rounded-xl p-3 shadow-md">Evidence</div>
-      </div>
-    </div>
-  </div>
-);
-
-const IndicatorNew = ({ validity }) => {
-  const [text, setText] = useState();
-  const [color, setColor] = useState();
-
-  useEffect(() => {
-    if (validity < 30) {
-      setText("False");
-      setColor("bg-red-500");
-    } else if (validity < 50) {
-      setText("Likely False");
-      setColor("bg-red-300");
-    } else if (validity < 80) {
-      setText("Likely True");
-      setColor("bg-green-300");
-    } else {
-      setText("True");
-      setColor("bg-green-500");
-    }
-  }, [validity]);
-
-  return <div className={`${color} p-2 rounded-2xl`}>{text}</div>;
-};
