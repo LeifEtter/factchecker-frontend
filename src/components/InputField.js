27a94692a7c4438c1a8f259d7e7cbf23a330eaed
Leif Etter <leif.etter@gmx.de>
2023-04-27 01:08:02 +0200

    extract input field into seperate file

---
+++ src/components/InputField.js
@@ -0,0 +1,29 @@
+export const InputField = ({
+  value,
+  setValue,
+  title,
+  error,
+  resetError,
+  obscure,
+}) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <p className="ml-1 font-semibold text-fact-text-medium">{title}</p>
+      <input
+        type={obscure ? "password" : "text"}
+        value={value}
+        onChange={(e) => {
+          if (error != null) {
+            resetError();
+          }
+          setValue(e.target.value);
+        }}
+        className="bg-white rounded-2xl focus:outline-1 outline-blue-400 py-2 px-4 special-shadow"
+        style={{
+          border: error != null ? "2px solid red" : "none",
+        }}
+      />
+      <p className="text-red-500">{error}</p>
+    </div>
+  );
+};
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,3 +1,4 @@
+import { InputField } from "@/components/InputField";
 import { isEmail, isPassword } from "@/helpers/helpers";
 import Head from "next/head";
 import { useEffect, useState } from "react";
@@ -93,26 +94,3 @@ export default function Register() {
     </>
   );
 }
-
-const InputField = ({ value, setValue, title, error, resetError, obscure }) => {
-  return (
-    <div className="flex flex-col gap-1">
-      <p className="ml-1 font-semibold text-fact-text-medium">{title}</p>
-      <input
-        type={obscure ? "password" : "text"}
-        value={value}
-        onChange={(e) => {
-          if (error != null) {
-            resetError();
-          }
-          setValue(e.target.value);
-        }}
-        className="bg-white rounded-2xl focus:outline-1 outline-blue-400 py-2 px-4 special-shadow"
-        style={{
-          border: error != null ? "2px solid red" : "none",
-        }}
-      />
-      <p className="text-red-500">{error}</p>
-    </div>
-  );
-};
