96d1494893f2f45fe01cc315efb201196f1c960c
Leif Etter <leif.etter@gmx.de>
2023-04-28 00:09:55 +0200

    add validation to claim creation

--- src/pages/create-claim.js
+++ src/pages/create-claim.js
@@ -15,9 +15,8 @@ export default function CreateClaim() {
 
   const [images, setImages] = useState([]);
 
-  const [currentImage, setCurrentImage] = useState();
-
-  const [sources, setSources] = useState([]);
+  const [source, setSource] = useState("");
+  const [sourceError, setSourceError] = useState(null);
 
   const [showModal, setShowModal] = useState(false);
   const [modalInput, setModalInput] = useState();
@@ -48,56 +47,71 @@ export default function CreateClaim() {
   return (
     <>
       <Head></Head>
-      <div>
-        <Modal
-          showModal={showModal}
-          setModalInput={setModalInput}
-          setShowModal={setShowModal}
-        />
-        <h1 className="font-bold text-2xl text-fact-text-medium text-center mb-5 mt-24">
-          Create Claim
-        </h1>
-        <InputField
-          value={title}
-          setValue={setTitle}
-          title="Title"
-          error={titleError}
-          resetError={() => setTitleError(null)}
-        />
-        <InputFieldMultiline
-          value={description}
-          setValue={setDescription}
-          title="Description"
-          error={descriptionError}
-          resetError={() => setDescriptionError(null)}
-        />
-        <div>
-          <p className="ml-1 font-semibold text-fact-text-medium">Images</p>
-          <div className="flex items-center gap-5">
-            {images.map((image) => (
-              <div
-                className="relative -z-10 w-48 h-48 bg-white rounded-2xl special-shadow"
-                key={image}
+      <Modal
+        showModal={showModal}
+        setModalInput={setModalInput}
+        setShowModal={setShowModal}
+      />
+      <main className="flex justify-center">
+        <div className="w-11/12 max-w-md flex flex-col gap-3">
+          <h1 className="font-bold text-2xl text-fact-text-medium text-center mb-5 mt-24">
+            Create Claim
+          </h1>
+          <InputField
+            value={title}
+            setValue={setTitle}
+            title="Claim"
+            error={titleError}
+            resetError={() => setTitleError(null)}
+          />
+          <InputFieldMultiline
+            value={description}
+            setValue={setDescription}
+            title="Description"
+            error={descriptionError}
+            resetError={() => setDescriptionError(null)}
+          />
+          <div>
+            <p className="ml-1 font-semibold text-fact-text-medium">Images</p>
+            <div className="flex items-center gap-5">
+              {images.map((image) => (
+                <div
+                  className="relative -z-10 w-48 h-48 bg-white rounded-2xl special-shadow"
+                  key={image}
+                >
+                  <Image
+                    src={image}
+                    alt={image}
+                    fill
+                    className="object-cover rounded-2xl"
+                  />
+                </div>
+              ))}
+              <button
+                className="flex p-4 mt-2 bg-white rounded-2xl special-shadow"
+                onClick={() => setShowModal(true)}
               >
-                <Image
-                  src={image}
-                  alt={image}
-                  fill
-                  className="object-cover rounded-2xl"
-                />
-              </div>
-            ))}
-            <button
-              className="flex p-5 bg-white rounded-2xl special-shadow"
-              onClick={() => setShowModal(true)}
-            >
-              <FontAwesomeIcon icon={faAdd} size="xl" />
-            </button>
+                <FontAwesomeIcon icon={faAdd} size="lg" />
+              </button>
+            </div>
           </div>
-        </div>
 
-        <button onClick={() => {}}>Submit</button>
-      </div>
+          <InputField
+            value={source}
+            setValue={setSource}
+            title="Source"
+            error={sourceError}
+            resetError={() => setSourceError(null)}
+          />
+
+          <button
+            className="special-shadow fact-gradient rounded-xl text-white p-3 w-full"
+            onClick={() => {}}
+          >
+            Submit
+          </button>
+        </div>
+      </main>
     </>
   );
 }
@@ -105,6 +119,15 @@ export default function CreateClaim() {
 const Modal = ({ showModal, setModalInput, setShowModal }) => {
   const [imageUrl, setImageUrl] = useState("");
   const [imageFile, setImageFile] = useState();
+  const [error, setError] = useState();
+
+  const validate = () => {
+    if (imageUrl == "" || imageFile == null) {
+      setError(true);
+      return false;
+    }
+    return true;
+  };
 
   return showModal ? (
     <div>
@@ -114,15 +137,16 @@ const Modal = ({ showModal, setModalInput, setShowModal }) => {
           setImageUrl("");
           setModalInput(null);
           setShowModal(false);
+          setError(false);
         }}
         className="fixed flex top-0 justify-center w-full h-full bg-gray-500 opacity-50"
       />
       <div className="fixed top-0 flex justify-center w-full">
-        <div className="flex flex-col items-center fixed z-10 blue-background top-96 p-5 rounded-2xl">
+        <div className="flex flex-col items-center fixed z-10 blue-background top-96 p-5 rounded-2xl w-full max-w-sm">
           <h2 className="text-xl font-semibold mb-5">Add Image</h2>
           <p>Source:</p>
           <input
-            className="special-shadow bg-white rounded-md p-1 mt-1"
+            className="special-shadow bg-white rounded-md py-1 px-2 mt-1 w-full"
             type="text"
             value={imageUrl}
             onChange={(e) => setImageUrl(e.target.value)}
@@ -140,9 +164,20 @@ const Modal = ({ showModal, setModalInput, setShowModal }) => {
             onChange={(e) => setImageFile(e.target.files[0])}
             className="hidden"
           />
+          {error ? (
+            <p className="text-sm text-red-500 mt-2">
+              Please select an image and source
+            </p>
+          ) : (
+            <></>
+          )}
+
           <button
             className="fact-gradient p-2 text-white rounded-xl special-shadow mt-5"
             onClick={() => {
+              if (!validate()) {
+                return;
+              }
               setShowModal(false);
               setModalInput({
                 imageUrl: imageUrl,
@@ -150,6 +185,7 @@ const Modal = ({ showModal, setModalInput, setShowModal }) => {
               });
               setImageUrl("");
               setImageFile(null);
+              setError(false);
             }}
           >
             Submit Image
