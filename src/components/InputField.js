f5a9dac746563c7f256dbd8a1eebdb6bf1c729ef
Leif Etter <leif.etter@gmx.de>
2023-04-27 18:31:17 +0200

    add multiline input field

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -27,3 +27,31 @@ export const InputField = ({
     </div>
   );
 };
+
+export const InputFieldMultiline = ({
+  value,
+  setValue,
+  title,
+  error,
+  resetError,
+}) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <p className="ml-1 font-semibold text-fact-text-medium">{title}</p>
+      <textarea
+        value={value}
+        onChange={(e) => {
+          if (error != null) {
+            resetError();
+          }
+          setValue(e.target.value);
+        }}
+        className="bg-white rounded-2xl focus:outline-1 outline-blue-400 py-2 px-4 special-shadow"
+        style={{
+          border: error != null ? "2px solid red" : "none",
+        }}
+      />
+      <p className="text-red-500">{error}</p>
+    </div>
+  );
+};
