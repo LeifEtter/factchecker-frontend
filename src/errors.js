eb5829fddc092ff418295061e72e98097b16a1fc
Leif Etter <leif.etter@gmx.de>
2023-04-27 02:16:29 +0200

    add error codes

---
+++ src/errors.js
@@ -0,0 +1,10 @@
+export const CustomErrors = {
+  UnknownError: 0,
+  InvalidInput: 1,
+  EmailNotExist: 2,
+  InvalidPassword: 3,
+  EmailWrongFormat: 4,
+  PasswordWrongFormat: 5,
+  NameMissing: 6,
+  EmailAlreadyExists: 7,
+};
