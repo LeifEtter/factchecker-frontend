3e5862ab29199fdd9ed88e21d10791a6b5e9cc9a
Leif Etter <leif.etter@gmx.de>
2023-04-27 17:16:17 +0200

    extract appbar into seperate file

---
+++ src/components/Appbar.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+export const Appbar = ({ path, token }) => (
+  <div className="mt-5 ml-5 mr-5 flex justify-between">
+    <div className="flex justify-between bg-white rounded-2xl shadow-md float-left gap-5 py-2 px-3">
+      <LinkButton path="/" title="Home" isActive={path == "/"} />
+      <LinkButton path="/new" title="New" isActive={path == "/new"} />
+      <LinkButton
+        path="/requests"
+        title="Requests"
+        isActive={path == "/requests"}
+      />
+    </div>
+    <div className="flex justify-between bg-white rounded-2xl shadow-md float-left gap-10 py-2 px-3">
+      {!token ? (
+        <LinkButton
+          path="/login"
+          title="Login/Register"
+          isActive={path == "/login" || path == "/register"}
+        />
+      ) : (
+        <UserBar></UserBar>
+      )}
+    </div>
+  </div>
+);
+
+const LinkButton = ({ title, path, isActive }) => {
+  return (
+    <Link
+      href={path}
+      className="py-1 px-2 rounded-xl font-semibold"
+      style={{
+        backgroundColor: isActive ? "rgb(239, 237, 237)" : "rgba(0,0,0,0)",
+        color: isActive ? "black" : "#535353",
+      }}
+    >
+      {title}
+    </Link>
+  );
+};
+
+const UserBar = () => {
+  return <></>;
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import { Appbar } from "@/components/Appbar";
 import "@/styles/globals.css";
 import "@/styles/shadows.css";
 import { Roboto_Mono } from "next/font/google";
@@ -11,52 +12,10 @@ export default function App({ Component, pageProps }) {
   const [token, setToken] = useState(null);
   const path = useRouter().pathname;
 
-  const UserBar = () => {
-    <div># Avatar</div>;
-  };
-
   return (
     <main className={robotoMono.className}>
-      <div className="mt-5 ml-5 mr-5 flex justify-between">
-        <div className="flex justify-between bg-white rounded-2xl shadow-md float-left gap-5 py-2 px-3">
-          <LinkButton path="/" title="Home" isActive={path == "/"} />
-
-          <LinkButton path="/new" title="New" isActive={path == "/new"} />
-          <LinkButton
-            path="/requests"
-            title="Requests"
-            isActive={path == "/requests"}
-          />
-        </div>
-        <div className="flex justify-between bg-white rounded-2xl shadow-md float-left gap-10 py-2 px-3">
-          {!token ? (
-            <LinkButton
-              path="/login"
-              title="Login/Register"
-              isActive={path == "/login" || path == "/register"}
-            />
-          ) : (
-            <UserBar></UserBar>
-          )}
-        </div>
-      </div>
-
+      <Appbar path={path} token={token} />
       <Component {...pageProps} />
     </main>
   );
 }
-
-const LinkButton = ({ title, path, isActive }) => {
-  return (
-    <Link
-      href={path}
-      className="py-1 px-2 rounded-xl font-semibold"
-      style={{
-        backgroundColor: isActive ? "rgb(239, 237, 237)" : "none",
-        color: isActive ? "black" : "#535353",
-      }}
-    >
-      {title}
-    </Link>
-  );
-};
