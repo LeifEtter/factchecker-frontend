c8faa3ee9a8942d48671d9f6fc757bb67a79cd4e
Leif Etter <leif.etter@gmx.de>
2023-04-27 17:10:15 +0200

    remove previous appbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <div className="flex flex-col w-full max-w-4xl">
-        <div className="mt-5 ml-5 mr-5">
-          <div className="flex justify-between bg-white rounded-2xl shadow-md px-3 py-3 float-left gap-10">
-            <Link href="/">Home</Link>
-            <Link href="/">New</Link>
-            <Link href="/">Requests</Link>
-          </div>
-        </div>
         <div>
           <h1 className="text-lg mt-5 ml-5">Posts/Articles</h1>
         </div>
