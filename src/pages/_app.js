cbef3ad88ebb83ce09d41b5d3c878bee1059f5de
Leif Etter <leif.etter@gmx.de>
2023-04-28 02:36:16 +0200

    add validation of token if present in cookies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { Appbar } from "@/components/Appbar";
 import { TokenContext } from "@/state/token";
 import "@/styles/globals.css";
 import "@/styles/shadows.css";
+import Cookies from "js-cookie";
 import { Roboto_Mono } from "next/font/google";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -12,6 +13,27 @@ const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   const path = useRouter().pathname;
   const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = Cookies.get("auth_token");
+    checkToken(token);
+  }, []);
+
+  const checkToken = async (token) => {
+    const result = await fetch("http://localhost:3005/users/authenticate", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (result.status == 200) {
+      setToken(token);
+    } else {
+      setToken(null);
+      Cookies.remove("auth_token");
+    }
+  };
 
   return (
     <main className={robotoMono.className}>
