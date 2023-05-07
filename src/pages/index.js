655eabeba0056452d4a5ab915c9703012f11ebaf
Leif Etter <leifetter@Leifs-MacBook-Pro-2.local>
2023-05-07 04:34:56 +0200

    add images to claim cards in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
-import { ClaimCard } from "@/components/ClaimCard";
+import { ClaimCard, ClaimCardWithImage } from "@/components/ClaimCard";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+
+const EXAMPLE_IMAGE =
+  "https://factchecker-images.s3.eu-central-1.amazonaws.com/6f409519-6546-4d6d-987d-2cddeabfac8b";
 
 export default function Home() {
   const [claims, setClaims] = useState([]);
@@ -22,13 +25,14 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center">
-      <div className="flex flex-col w-full max-w-4xl">
+      <div className="flex flex-col w-full max-w-4xl text-c">
         <div>
           <h1 className="text-lg mt-5 ml-5">Posts/Articles</h1>
         </div>
         <div className="grid grid-cols-3">
           {claims.map((claim) => (
-            <ClaimCard
+            <ClaimCardWithImage
+              images={[EXAMPLE_IMAGE, EXAMPLE_IMAGE]}
               statement={claim.statement}
               description={claim.description}
               source={claim.source}
