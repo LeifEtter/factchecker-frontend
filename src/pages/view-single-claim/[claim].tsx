4a356066304aab09df2c1c8b50fd5fbc9f8881ad
Leif Etter <leif.etter@gmx.de>
2023-09-12 17:39:03 +0200

    improve styling; hide comment section when no comments have been made;

--- src/pages/view-single-claim/[claim].tsx
+++ src/pages/view-single-claim/[claim].tsx
@@ -24,6 +24,11 @@ export default function ViewSingleClaim({ claim }: ViewSingleClaimProps) {
     return totalTruthPoints;
   };
 
+  const gradients = {
+    red: "from-fact-red-gr-1 to-fact-red-gr-2",
+    green: "from-fact-green-gr-1 to-fact-green-gr-2",
+  };
+
   useEffect(() => {
     const truthFactor: number = calculateTruthFactorFromComments(
       claim.comments
@@ -45,36 +50,39 @@ export default function ViewSingleClaim({ claim }: ViewSingleClaimProps) {
 
   return (
     <div>
-      <h1 className="mt-10 mb-3">Claim</h1>
+      <h1 className="mt-10 mb-3 text-xl font-bold">CLAIM</h1>
       <div className="bg-white rounded-xl shadow-xl px-6 md:px-16 py-10 w-full">
         <div className="flex flex-row">
           <div>
-            <h2>{claim.statement}</h2>
+            <h2 className="text-xl">{claim.statement}</h2>
           </div>
-          <div className="flex-grow"></div>
+          <div className="flex-grow" />
           <SourceButton link={"nothing"} />
           <TruthFactorLabel label={truthLabel} value={truthValue} />
         </div>
         <p className="mt-3">{claim.description}</p>
-        <div className="basis-3/12 w-full flex flex-row h-56 md:h-72 gap-4 md:gap-8 mt-16">
-          {claim.images.map((image) => (
-            <div key={`${image.id}-image`} className="flex-1 relative">
-              <Image
-                src={image.link}
-                alt={image.id.toString()}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                className="object-cover rounded-2xl"
-              />
-            </div>
-          ))}
-        </div>
+        {claim.images.length > 0 ? (
+          <div className="basis-3/12 w-full flex flex-row h-56 md:h-72 gap-4 md:gap-8 mt-16">
+            {claim.images.map((image) => (
+              <div key={`${image.id}-image`} className="flex-1 relative">
+                <Image
+                  src={image.link}
+                  alt={image.id.toString()}
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="object-cover rounded-2xl"
+                />
+              </div>
+            ))}
+          </div>
+        ) : null}
       </div>
       {claim.comments.length > 1 ? <h1 className="mt-16">Statements</h1> : null}
       {claim.comments.map((comment) => (
         <div
-          className="mt-5 w-full rounded-2xl p-5 bg-white shadow-lg"
-          style={{ backgroundColor: comment.result ? "green" : "red" }}
+          className={`mt-5 w-full rounded-2xl p-5 bg-white shadow-lg bg-gradient-to-br ${
+            gradients[comment.result ? "green" : "red"]
+          }`}
           key={`comment-${comment.id}`}
         >
           {comment.statement}
