020a22bca13bf04e88e83ef46d114ca9f085ec23
Leif Etter <leif.etter@gmx.de>
2023-10-31 00:50:55 +0100

    uninstall js-cookie

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { MouseEventHandler, useContext } from "react";
 import { UserContext } from "../state/user";
-import Cookies from "js-cookie";
 
 interface AppbarParams {
   path: string;
