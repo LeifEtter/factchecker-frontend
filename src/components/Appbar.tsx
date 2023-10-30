9df07f804b29654b797935bd4513e104daa993b5
Leif Etter <leif.etter@gmx.de>
2023-10-31 00:48:39 +0100

    move up group class in userbar and remove unnecessary div; move logout function into seperate method; push to index on logout

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { MouseEventHandler, useContext } from "react";
 import { UserContext } from "../state/user";
+import Cookies from "js-cookie";
 
 interface AppbarParams {
   path: string;
@@ -13,6 +14,17 @@ export const Appbar = ({ path, user }: AppbarParams) => {
   const { setUser } = useContext(UserContext);
 
   const router = useRouter();
+
+  const logout = async () => {
+    await fetch("http://localhost:3005/users/logout", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    });
+    sessionStorage.removeItem("user");
+    setUser(null);
+    router.push("/");
+  };
 
   return (
     <>
@@ -40,13 +52,7 @@ export const Appbar = ({ path, user }: AppbarParams) => {
               <UserBar
                 user={user}
                 avatarClick={() => router.push("/profile")}
-                logout={() => {
-                  sessionStorage.removeItem("user");
-                  setUser(null);
-
-                  document.cookie =
-                    "jwt_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                }}
+                logout={logout}
               ></UserBar>
             </>
           ) : (
@@ -101,27 +107,25 @@ const UserBar = ({ user, avatarClick, logout }: UserBarParams) => {
   return (
     <>
       <p>{user.name}</p>
-      <button className="relative w-10 h-10" onClick={avatarClick}>
-        <div className="group">
-          <Image
-            src={user.avatar}
-            priority
-            alt={`avatar-image-appbar`}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-cover rounded-full special-shadow"
-          />
-          <div className="absolute w-32 -ml-12 h-12" />
-          <div className="absolute flex group-hover:flex flex-col special-shadow bg-white mt-8 -ml-20 w-32 rounded-xl py-3 gap-3 [&>*]:duration-300 [&>*]:ease-in-out [&>*]:mx-4 [&>*]:rounded-md">
-            <button className="hover:bg-blue-100" onClick={avatarClick}>
-              Profile
-            </button>
-            <button className="hover:bg-blue-100" onClick={logout}>
-              Sign Out
-            </button>
-          </div>
+      <div className="group relative w-10 h-10" onClick={avatarClick}>
+        <Image
+          src={user.avatar}
+          priority
+          alt={`avatar-image-appbar`}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="object-cover rounded-full special-shadow"
+        />
+        <div className="absolute w-32 -ml-12 h-12" />
+        <div className="absolute hidden group-hover:flex flex-col special-shadow bg-white mt-8 -ml-20 w-32 rounded-xl py-3 gap-3 [&>*]:duration-300 [&>*]:ease-in-out [&>*]:mx-4 [&>*]:rounded-md">
+          <button className="hover:bg-blue-100" onClick={avatarClick}>
+            Profile
+          </button>
+          <button className="hover:bg-blue-100" onClick={logout}>
+            Sign Out
+          </button>
         </div>
-      </button>
+      </div>
     </>
   );
 };
