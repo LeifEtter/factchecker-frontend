3ae10bd8df3e00251c335b2e20007565f66d205a
Leif Etter <leif.etter@gmx.de>
2023-11-14 18:38:52 +0100

    add lorem ipsum object to test file to later generate text; add function for creating list of claims for mocking; mock the user score hook and use mockedclaims in it;

--- src/pages/scores/scores.test.tsx
+++ src/pages/scores/scores.test.tsx
@@ -1,6 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import Scores from "./[id]";
 import "@testing-library/jest-dom/";
+import { LoremIpsum } from "lorem-ipsum";
+import { randomInt } from "crypto";
+import { ScoreData } from "../../hooks/useScoreData";
+
+const lorem = new LoremIpsum({
+  wordsPerSentence: {
+    min: 5,
+    max: 16,
+  },
+});
+
+const mockClaims: Claim[] = Array(10)
+  .fill(0)
+  .map((_) => {
+    const id: number = randomInt(999999);
+    return {
+      id,
+      source: lorem.generateWords(3),
+      statement: lorem.generateWords(10),
+      description: lorem.generateWords(50),
+      images: [
+        {
+          id,
+          link: lorem.generateWords(1),
+          source: lorem.generateWords(2),
+          claimId: 0,
+        },
+      ],
+    };
+  });
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -13,7 +43,12 @@ jest.mock("next/router", () => ({
 }));
 
 jest.mock("../../hooks/useScoreData", () => ({
-  useScoreData: () => [],
+  useScoreData: (): ScoreData => ({
+    claimsCreated: mockClaims,
+    commentsCreated: [],
+    upvotesReceived: 10,
+    downvotesReceived: 5,
+  }),
 }));
 
 describe("should test the scores page", () => {
