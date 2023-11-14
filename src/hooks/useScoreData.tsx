a9f1b7dd02349305c46109eb87d94be3beee6b1a
Leif Etter <leif.etter@gmx.de>
2023-11-14 17:40:42 +0100

    export scoredata interface to be usable in other files

--- src/hooks/useScoreData.tsx
+++ src/hooks/useScoreData.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-interface ScoreData {
+export interface ScoreData {
   claimsCreated: Claim[];
   commentsCreated: Comment[];
   upvotesReceived: number;
