147cbe007f63d65926621bdd7bcb31a5ec3d5c23
Leif Etter <leif.etter@gmx.de>
2023-11-14 18:23:12 +0100

    change scores page testing file to .tsx; add test to test number of children in claim list

--- src/pages/scores/scores.test.tsx
+++ src/pages/scores/scores.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Scores from "./[id]";
 import "@testing-library/jest-dom/";
 
@@ -12,8 +12,14 @@ jest.mock("next/router", () => ({
   },
 }));
 
+jest.mock("../../hooks/useScoreData", () => ({
+  useScoreData: () => [],
+}));
+
 describe("should test the scores page", () => {
   it("should test the basic page setup", () => {
     render(<Scores />);
+    const claimList = screen.getByTestId("claim-list");
+    expect(claimList.children).toHaveLength(10);
   });
 });
