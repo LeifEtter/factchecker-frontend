2aa8b1e6ff4031086d055e6eed79d88e14268061
Leif Etter <leif.etter@gmx.de>
2023-11-14 19:49:42 +0100

    show user level on page

--- src/pages/scores/[id].tsx
+++ src/pages/scores/[id].tsx
@@ -26,6 +26,7 @@ export default function Scores() {
   return (
     <>
       <h1>Claims</h1>
+      <p>User Level: {userLevel}</p>
       <div data-testid="claim-list">
         {scoreData.claimsCreated.map((claim) => (
           <div key={`claim-${claim.id}`}>
