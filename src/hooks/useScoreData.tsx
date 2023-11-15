01da236db8fcba7478576232f1c292da0bb7d2c8
Leif Etter <leif.etter@gmx.de>
2023-11-15 17:26:17 +0100

    add userid as dependency into custom hooks' useeffect function and only fetch if userid does not equal null

--- src/hooks/useScoreData.tsx
+++ src/hooks/useScoreData.tsx
@@ -11,25 +11,27 @@ export function useScoreData(userId: string) {
   const [scoreData, setScoreData] = useState<null | ScoreData>(null);
 
   useEffect(() => {
-    try {
-      fetch(`http://localhost:3005/users/scores/${userId}`, {
-        method: "GET",
-        mode: "cors",
-        credentials: "include",
-      })
-        .then((res) => res.json())
-        .then((scores) => {
-          setScoreData({
-            claimsCreated: scores.claimsCreated,
-            commentsCreated: scores.commentsCreated ?? [],
-            upvotesReceived: scores.upvotesReceived,
-            downvotesReceived: scores.downvotesReceived,
+    if (userId != null) {
+      try {
+        fetch(`http://localhost:3005/users/scores/${userId}`, {
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+        })
+          .then((res) => res.json())
+          .then((scores) => {
+            setScoreData({
+              claimsCreated: scores.claimsCreated,
+              commentsCreated: scores.commentsCreated ?? [],
+              upvotesReceived: scores.upvotesReceived,
+              downvotesReceived: scores.downvotesReceived,
+            });
           });
-        });
-    } catch (error) {
-      console.error(error);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }, []);
+  }, [userId]);
 
   return scoreData;
 }
--- src/hooks/useUserDetails.tsx
+++ src/hooks/useUserDetails.tsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
 
 export function useUserDetails(userId: string): User {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User>(null);
 
   useEffect(() => {
-    try {
-      fetch(`http://localhost:3005/users/profile/${userId}`, {
-        method: "GET",
-        mode: "cors",
-        credentials: "include",
-      })
-        .then((res) => res.json())
-        .then((user) => setUser(user));
-    } catch (error) {
-      console.error(error);
+    if (userId != null) {
+      try {
+        fetch(`http://localhost:3005/users/profile/${userId}`, {
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+        })
+          .then((res) => res.json())
+          .then((user) => setUser(user));
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }, []);
+  }, [userId]);
 
   return user;
 }
