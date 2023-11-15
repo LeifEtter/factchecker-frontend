0e4dd1ae366d1a1584d62bc16e50cf9cf991dd29
Leif Etter <leif.etter@gmx.de>
2023-11-15 15:10:55 +0100

    remove old indicator containing user level

--- src/pages/scores/[id].tsx
+++ src/pages/scores/[id].tsx
@@ -70,7 +70,6 @@ export default function Scores() {
       </div>
       <p data-testid="user-biography">{user.biography}</p>
       <h1>Claims</h1>
-      <p data-testid="user-level">User Level: {userLevel}</p>
       <div data-testid="claim-list">
         {scoreData.claimsCreated.map((claim) => (
           <div key={`claim-${claim.id}`}>
