f552034d7f12c48d087a6b3843d1bcb1b9727c4b
Leif Etter <leif.etter@gmx.de>
2023-11-16 16:18:08 +0100

    use scoredataloading boolean instead of scoredata null check in useeffect hook to control level and user title initialization

--- src/pages/scores/[id].tsx
+++ src/pages/scores/[id].tsx
@@ -77,7 +77,7 @@ export default function Scores() {
     if (isAuthenticated == false) {
       router.push("/login");
     }
-    if (scoreData != null) {
+    if (!scoreDataIsLoading) {
       const level = calculateLevelFromScoreData(scoreData);
       setUserLevel(level);
       const userTitle = determineUserTitleFromLevel(level);
