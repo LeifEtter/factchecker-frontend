603d1cef2574217ddd04a6efba781bc2ea8ece49
Leif Etter <leif.etter@gmx.de>
2023-11-16 03:21:33 +0100

    style the show all and hide buttons for claims list

--- src/pages/scores/[id].tsx
+++ src/pages/scores/[id].tsx
@@ -148,9 +148,19 @@ export default function Scores() {
           {user.name.split(" ")[0]}s Claims
         </h1>
         {expandClaims ? (
-          <button onClick={() => setExpandClaims(false)}>Hide</button>
+          <button
+            onClick={() => setExpandClaims(false)}
+            className="text-blue-600 text-xs font-medium"
+          >
+            Hide
+          </button>
         ) : (
-          <button onClick={() => setExpandClaims(true)}>Show All</button>
+          <button
+            onClick={() => setExpandClaims(true)}
+            className="text-blue-600 text-xs font-medium"
+          >
+            Show All
+          </button>
         )}
       </div>
       <div
