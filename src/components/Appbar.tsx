8ed7aba6846880582e2b57a7e7883941aa8e0e58
Leif Etter <leif.etter@gmx.de>
2023-11-16 23:36:33 +0100

    remove 'new' button from appbar

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -31,7 +31,6 @@ export const Appbar = ({ path, user }: AppbarParams) => {
       <div className="mt-5 flex justify-between">
         <div className="flex justify-between bg-white rounded-2xl shadow-md float-left gap-5 py-2 px-3">
           <LinkButton path="/" title="Home" isActive={path == "/"} />
-          <LinkButton path="/new" title="New" isActive={path == "/new"} />
           <LinkButton
             path="/requests"
             title="Requests"
