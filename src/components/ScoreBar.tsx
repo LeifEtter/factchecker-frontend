59a1fe840bc188c137c7c77e3217efbbde02fc1b
Leif Etter <leif.etter@gmx.de>
2023-11-16 02:40:31 +0100

    remove margin and min height and change padding to allow better styling of scorebar

--- src/components/ScoreBar.tsx
+++ src/components/ScoreBar.tsx
@@ -16,7 +16,7 @@ export const ScoreBar = ({
   scoreData,
   userLevel,
 }: ScoreBarProps): React.ReactElement => (
-  <div className="h-10 bg-white special-shadow rounded-4xl flex items-center p-5 gap-3 justify-center mt-12 font-semibold text-gray-800">
+  <div className=" bg-white special-shadow rounded-4xl flex items-center px-6 py-1 gap-3 justify-center font-semibold text-gray-800">
     <div className="flex gap-1 items-center">
       <h3 data-testid="indicator-claims-created">
         {scoreData.claimsCreated.length}
