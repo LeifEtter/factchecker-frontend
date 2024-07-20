1f9f051495f73cccd44e2ecd89bae452aa02ebc9
Leif Etter <leif.etter@gmx.de>
2024-07-21 00:38:49 +0200

    pass success message to login screen as query param

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,8 +4,11 @@ import { SnackBar, SnackbarType } from "../components/Snackbar";
 import { CustomErrors } from "../types/errors";
 import { isEmail, isPassword } from "../helpers/helpers";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Register() {
+  const router = useRouter();
+
   const [name, setName] = useState("");
   const [nameError, setNameError] = useState(null);
 
@@ -75,6 +78,9 @@ export default function Register() {
       if (body.errorCode == CustomErrors.EmailAlreadyExists) {
         setEmailError("Email Does Already Exist");
       }
+    } else {
+      clearAllFields();
+      router.push({ pathname: "/login", query: { popup: "Login Successful" } });
     }
   };
 
