268e8b204cbcf7977973b11f95ccabb978ab7a54
Leif Etter <leif.etter@gmx.de>
2024-07-24 22:17:50 +0200

    redirect to home page on successful login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -56,7 +56,7 @@ export default function Login() {
     } else {
       sessionStorage.setItem("user", JSON.stringify(body.user));
       setUser(body.user);
-      router.back();
+      router.push("/");
     }
   };
 
