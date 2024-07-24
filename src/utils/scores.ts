42493ea385e030c357acee865a9ada9935854af9
Leif Etter <leif.etter@gmx.de>
2024-07-24 22:09:30 +0200

    reduce value calculated as user level by reducing multipliers

--- src/utils/scores.ts
+++ src/utils/scores.ts
@@ -3,8 +3,8 @@ import { ScoreData } from "../hooks/useScoreData";
 
 export const calculateLevelFromScoreData = (scoreData: ScoreData): number => {
   return (
-    scoreData.claimsCreated.length * 5 +
-    scoreData.commentsCreated.length * 8 +
+    scoreData.claimsCreated.length * 2 +
+    scoreData.commentsCreated.length * 5 +
     scoreData.upvotesReceived * 10 -
     scoreData.downvotesReceived * 10
   );
