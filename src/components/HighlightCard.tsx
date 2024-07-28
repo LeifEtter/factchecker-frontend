5965eb73550bf66bfb1aa912b4e66bd730d3ae01
Leif Etter <leif.etter@gmx.de>
2024-07-28 20:10:05 +0200

    replace dummy statement in highlighted comment with actual comment statement

--- src/components/HighlightCard.tsx
+++ src/components/HighlightCard.tsx
@@ -7,13 +7,7 @@ export const HighlightCard = ({ claim, comment }: HighlightCard) => (
   <div className="w-10/12 bg-white bg-opacity-100 rounded-3xl shadow-lg p-5 mt-5 flex flex-row gap-8 max-w-4xl">
     <div className="flex-1">
       <h2 className="font-bold text-lg">Highlighted Reason</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.
-      </p>
+      <p>{comment.statement}</p>
     </div>
     {/* <div className="w-32 border-red-500 border">graphic</div> */}
   </div>
