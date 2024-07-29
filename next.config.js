1139866467894a062b02c34755c64385bcd27343
Leif Etter <leif.etter@gmx.de>
2024-07-29 16:05:21 +0200

    remove unnecessary remotePattern addition in next config file

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
         protocol: "https",
         hostname: "factchecker-images.s3.eu-central-1.amazonaws.com",
       },
-      {
-        protocol: "http",
-        hostname: "ec2-18-153-69-44.eu-central-1.compute.amazonaws.com",
-      },
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
