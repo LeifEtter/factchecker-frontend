e46704f2f47ffbf9c8fa77aedb50c18e4cbb41e8
Leif Etter <leif.etter@gmx.de>
2024-11-02 13:50:37 +0100

    add loading indicator at bottom of page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -95,6 +95,12 @@ export default function Home() {
             <></>
           )}
         </div>
+        <div className="flex flex-row gap-2 mt-10" id="loading-dots">
+          <span className="sr-only">Loading...</span>
+          <div className="h-2 w-2 bg-gray-600 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+          <div className="h-2 w-2 bg-gray-600 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+          <div className="h-2 w-2 bg-gray-600 rounded-full animate-bounce"></div>
+        </div>
       </div>
     </main>
   );
