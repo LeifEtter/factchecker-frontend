82583424428d335c5d3307c0f5330a9bca6eab39
Leif Etter <leif.etter@gmx.de>
2024-11-12 20:50:11 +0100

    add cloudfront cdn url to remotePatterns; remove prev patterns

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    formats: ["image/webp"],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "factchecker-images.s3.eu-central-1.amazonaws.com",
-      },
-      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "d1mtefwdakcrcg.cloudfront.net" },
     ],
   },
 };
