fbb45adc5177fddceee336c256d4adc9ecd22bf7
Leif Etter <leif.etter@gmx.de>
2024-11-14 16:51:07 +0100

    create custom hook for fetching multiple users

---
+++ src/hooks/useFetchUserScores.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { API } from "../assets/constants";
+import { calculateLevelFromScoreData } from "../utils/scores";
+import { constructQueryUrl } from "../helpers/conversionHelpers";
+
+/**
+ * @param userId current users id
+ * @returns Hook used for getting the currently logged in users scores
+ */
+export function useFetchUserProfiles(
+  initialQuery: UserQuery
+): [UserQuery, Function, any[], boolean] {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [userQuery, setUserQuery] = useState<UserQuery>(initialQuery);
+  const [users, setUsers] = useState<any[]>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const queryString = constructQueryUrl(userQuery);
+    try {
+      fetch(`${API}/${queryString}`, {
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((newUsers: any[]) => {
+          for (let user of newUsers) {
+            user.level = calculateLevelFromScoreData({
+              claimsCreated: user["claims_created"],
+              commentsCreated: user["comments_created"],
+              upvotesReceived: user["upvotesReceived"],
+              downvotesReceived: user["downvotesReceived"],
+            });
+          }
+          newUsers.sort((a, b) => b.level - a.level);
+          if (users && users.length != 0 && userQuery.skip != 0) {
+            setUsers([...users, ...newUsers]);
+          } else {
+            setUsers(newUsers);
+          }
+          setIsLoading(false);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }, [userQuery]);
+  return [userQuery, setUserQuery, users, isLoading];
+}
