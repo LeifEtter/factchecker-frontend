34c700833504014f13342c8b58d7a75ed81efd47
Leif Etter <leif.etter@gmx.de>
2024-11-14 16:52:02 +0100

    extract sorting option constants into assets folder

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,3 +1,15 @@
 export const API = process.env.NEXT_PUBLIC_API_URL;
 // export const API = "ec2-18-159-108-204.eu-central-1.compute.amazonaws.com:8080";
 export const PLACEHOLDER_DATE = "Joined November 2013";
+
+export const SORTING_OPTIONS = [
+  "comment_amount",
+  "creation_date",
+  "controversial",
+];
+
+export const SORTING_OPTION_LABELS = {
+  creation_date: "Date Created",
+  comment_amount: "Popularity",
+  controversial: "Controversial",
+};
