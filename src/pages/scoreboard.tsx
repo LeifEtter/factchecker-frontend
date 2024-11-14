7c2f4ede8e303cd40665e63ada4812d4b2e3def3
Leif Etter <leif.etter@gmx.de>
2024-11-14 16:54:07 +0100

    rework scoreboard page to use custom hook for fetching user scores

--- src/pages/scoreboard.tsx
+++ src/pages/scoreboard.tsx
@@ -1,91 +1,40 @@
-import { useContext, useEffect, useState } from "react";
-import { API } from "../assets/constants";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronDown,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
+import { useContext, useEffect } from "react";
 import { RankCard } from "../components/cards/RankCard";
-import { UserContext } from "../state/user";
 import { useRouter } from "next/router";
 import DefaultAvatar from "../../assets/default_avatar.jpg";
-import { calculateLevelFromScoreData } from "../utils/scores";
 import { UserSettingsContext } from "../state/settings";
+import { useFetchUserProfiles } from "../hooks/useFetchUserScores";
+import { useScrollTracker } from "../hooks/useScrollTracker";
+import { LoadingDots } from "../components/LoadingDots";
+
+const USERS_PER_FETCH: number = 5;
 
 /**
  * @returns Page containing scores of the best 50 reviewers
  */
 export default function Scoreboard() {
-  const [userScores, setUserScores] = useState([]);
-  const { user, setUser } = useContext(UserContext);
   const { darkModeActive } = useContext(UserSettingsContext);
-  const [currentUsersScores, setCurrentUsersScores] = useState([]);
-  const [showingAll, setShowingAll] = useState<Boolean>(false);
   const router = useRouter();
 
-  useEffect(() => {
-    getFirstThreeUserScores();
-    if (user) {
-      getRequestingUsersScores();
+  const [userQuery, setUserQuery, users, isLoadingUsers] = useFetchUserProfiles(
+    {
+      endpoint: "users/scores",
+      limit: USERS_PER_FETCH,
+      skip: 0,
+      orderBy: "comments_created",
+      orderByDirection: "DESC",
     }
-  }, [user]);
+  );
 
-  const getFirstThreeUserScores = async () => {
-    const result = await fetch(
-      `${API}/users/scores?orderBy=comments_created&limit=3&skip=0`,
-      {
-        method: "GET",
-      }
-    );
-    if (result.status == 200) {
-      const userScores: Array<any> = await result.json();
-      for (let user of userScores) {
-        user.level = calculateLevelFromScoreData({
-          claimsCreated: user["claims_created"],
-          commentsCreated: user["comments_created"],
-          upvotesReceived: user["upvotesReceived"],
-          downvotesReceived: user["downvotesReceived"],
-        });
-      }
-      userScores.sort((a, b) => b.level - a.level);
-      setUserScores(userScores);
-    }
+  const onBottomReach = async () => {
+    if (users.length < userQuery.skip + USERS_PER_FETCH) return;
+    setUserQuery({
+      ...userQuery,
+      skip: (userQuery.skip += USERS_PER_FETCH),
+    });
   };
-
-  const getRestUserScores = async () => {
-    const result = await fetch(
-      `${API}/users/scores?orderBy=comments_created&limit=47&skip=3`,
-      {
-        method: "GET",
-      }
-    );
-    if (result.status == 200) {
-      const newUserScores: Array<any> = await result.json();
-      for (let user of newUserScores) {
-        user.level = calculateLevelFromScoreData({
-          claimsCreated: user["claims_created"],
-          commentsCreated: user["comments_created"],
-          upvotesReceived: user["upvotesReceived"],
-          downvotesReceived: user["downvotesReceived"],
-        });
-      }
-      newUserScores.sort((a, b) => b.level - a.level);
-      newUserScores.unshift(...userScores);
-      setUserScores(newUserScores);
-    }
-  };
-
-  const getRequestingUsersScores = async () => {
-    if (user) {
-      const result = fetch(`${API}/users/profile/${user.id}`, {
-        method: "GET",
-        mode: "cors",
-        credentials: "include",
-      })
-        .then((res) => res.json())
-        .then((res) => setCurrentUsersScores(result));
-    }
-  };
+  const [setTrackedElem] = useScrollTracker(users, onBottomReach);
+  useEffect(() => setTrackedElem(document.querySelector("#loading-dots")), []);
 
   return (
     <main
@@ -95,58 +44,35 @@ export default function Scoreboard() {
     >
       <h1 className="text-2xl font-medium mt-16 mb-10">Top Reviewers</h1>
       <div className="flex flex-col gap-6 max-w-md w-9/12">
-        {userScores.map((userScore, index) => {
-          if (!showingAll && index > 2) {
-            return;
-          }
-          let color: string;
-          if (index == 0) {
-            color = "251, 227, 129, 0.67";
-          } else if (index == 1) {
-            color = "192, 192, 192, 0.54";
-          } else if (index == 2) {
-            color = "205, 127, 50, 0.5";
-          } else {
-            color = "255, 255, 255, 1.0";
-          }
-          return (
-            <RankCard
-              key={`rank-${index}`}
-              userId={userScore["user_id"]}
-              rank={index}
-              name={userScore["user_name"]}
-              scores={userScore}
-              profileImage={userScore.avatar ?? DefaultAvatar}
-              backgroundColor={color}
-              onClick={() => router.push(`/scores/${userScore["user_id"]}`)}
-              level={userScore.level}
-            />
-          );
-        })}
-        {showingAll ? (
-          <div
-            onClick={() => setShowingAll(false)}
-            className="flex justify-center items-center gap-2 cursor-pointer"
-          >
-            <FontAwesomeIcon icon={faChevronDown} /> Show Less
-          </div>
-        ) : (
-          <div
-            onClick={() => {
-              setShowingAll(true);
-              getRestUserScores();
-            }}
-            className="flex justify-center items-center gap-2 cursor-pointer"
-          >
-            <FontAwesomeIcon icon={faChevronRight} /> Show More
-          </div>
-        )}
-
-        {/* <div className="w-full border"></div>{}
-        { user ? <div>
-          <h1 className="text-2xl font-medium text-center">Your Rank</h1>
-          <RankCard rank=
-        </div> : <></> } */}
+        {users &&
+          users.map((user, index) => {
+            let color: string;
+            if (index == 0) {
+              color = "251, 227, 129, 0.67";
+            } else if (index == 1) {
+              color = "192, 192, 192, 0.54";
+            } else if (index == 2) {
+              color = "205, 127, 50, 0.5";
+            } else {
+              color = "255, 255, 255, 1.0";
+            }
+            return (
+              <RankCard
+                key={`rank-${index}`}
+                userId={user["user_id"]}
+                rank={index}
+                name={user["user_name"]}
+                scores={user}
+                profileImage={user.avatar ?? DefaultAvatar}
+                backgroundColor={color}
+                onClick={() => router.push(`/scores/${user["user_id"]}`)}
+                level={user.level}
+              />
+            );
+          })}
+        <div className="flex justify-center pb-5">
+          <LoadingDots />
+        </div>
       </div>
     </main>
   );
