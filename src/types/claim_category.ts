15e896d3f72998e13409d1de2d74f00170845790
Leif Etter <leif.etter@gmx.de>
2024-11-14 16:48:14 +0100

    add interfaces for storing claim queries and categories

--- src/types/claim_category.ts
+++ src/types/claim_category.ts
@@ -2,3 +2,12 @@ interface ClaimCategory {
   id: number;
   name: string;
 }
+
+interface ClaimCategoryButtonData {
+  name: string;
+  active: boolean;
+}
+
+interface ClaimCategoryDict {
+  [Key: number]: ClaimCategoryButtonData;
+}
